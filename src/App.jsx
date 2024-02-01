e58ec47fe5e3529b1a41a42d847ba065c2902e9f
Miche <valemiche003@gmail.com>
2024-02-01 12:05:54 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import './App.css'
+import Home from './components/Home/Home'
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </>
+  )
+}
+
+export default App
